4c17bc5262414e02b084b30c8e26f38b49b5eefe
Alexandra Jurj <alexandrabuda06@gmail.com>
2018-03-20 11:36:20 +0200

    refactoring and fixed register view

--- spring-angular/src/main/resources/static/app/scripts/app.js
+++ spring-angular/src/main/resources/static/app/scripts/app.js
@@ -13,7 +13,7 @@ var states = [
         { name: 'login', state: { url: '/login', parent: 'base', templateUrl: 'views/login.html', controller: 'LoginCtrl', data: {text: "Login", visible: false } } },
         { name: 'dashboard', state: { url: '/dashboard', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'DashboardCtrl', data: {text: "Dashboard", visible: false } } },
         { name: 'overview', state: { url: '/overview', parent: 'dashboard', templateUrl: 'views/dashboard/overview.html', data: {text: "Overview", visible: true } } },
-        { name: 'reports', state: { url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html', data: {text: "Reports", visible: true } } },
+        { name: 'clients', state: { url: '/clients', parent: 'dashboard', templateUrl: 'views/dashboard/clientsclients.html', data: {text: "Clients", visible: true } } },
         { name: 'logout', state: { url: '/login', data: {text: "Logout", visible: true }} }
     ];
    
